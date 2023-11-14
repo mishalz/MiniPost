92e5bb0e100a01a99dc465c440a27f1b25f84bf4
Mishal Zulfiqar <67483068+mishalz@users.noreply.github.com>
2023-11-14 15:40:12 +0000

    Update posts.js path

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const Post = require('../models/post')
+const Post = require('../models/Post')
 
 // Create data
 router.post('/', async(req,res)=>{
